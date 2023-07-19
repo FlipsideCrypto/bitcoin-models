bbe6e3c4f3c73762309b73c3e39a0c1c80d5fdef
Jack Forgash <jmfxyz@pm.me>
2023-07-18 22:31:44 -0600

    add blocks

--- models/silver/streamline_ad_hoc/streamline__transactions_backfill.sql
+++ models/silver/streamline_ad_hoc/streamline__transactions_backfill.sql
@@ -3,57 +3,29 @@
     post_hook = if_data_call_function(
         func = "{{this.schema}}.udf_json_rpc(object_construct('sql_source', '{{this.identifier}}', 'external_table', 'transactions', 'exploded_key','[\"result\", \"tx\"]', 'producer_batch_size',100, 'producer_limit_size', 1000000, 'worker_batch_size',10))",
         target = "{{this.schema}}.{{this.identifier}}"
-    ),
-    enabled = False
+    )
 ) }}
 
-WITH last_3_days AS (
-    {% if var('STREAMLINE_RUN_HISTORY') %}
+WITH blocks AS (
 
     SELECT
-        0 AS block_number
-
-    {% else %}
-
+        ARRAY_CONSTRUCT(
+            91842,
+            91880,
+            798601,
+            798685,
+            798921
+        ) AS blist
+),
+tbl AS (
     SELECT
-        MAX(block_number) - 500 AS block_number --aprox 3 days
+        VALUE AS block_number
     FROM
-        {{ ref("bronze__streamline_blocks_hash") }}
-    {% endif %}),
-    tbl AS (
-        SELECT
-            block_number,
-            data:result::STRING AS block_hash
-        FROM
-            {{ ref("bronze__streamline_FR_blocks_hash") }}
-        WHERE
-            (
-                block_number >= (
-                    SELECT
-                        block_number
-                    FROM
-                        last_3_days
-                )
-            )
-            AND block_number <= 213840
-            AND block_number IS NOT NULL
-            AND block_number NOT IN (
-                SELECT
-                    block_number
-                FROM
-                    {{ ref("streamline__complete_transactions") }}
-                WHERE
-                    block_number >= (
-                        SELECT
-                            block_number
-                        FROM
-                            last_3_days
-                    )
-                    AND block_number IS NOT NULL
-                    AND block_number <= 213840
-
-            )
-    )
+        blocks,
+        LATERAL FLATTEN(
+            input => blist
+        )
+)
 SELECT
     block_number,
     'getblock' AS method,
