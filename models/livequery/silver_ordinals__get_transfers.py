030f64d0238e60334b5989057f05d75d5a3c8ceb
Jack Forgash <58153492+forgxyz@users.noreply.github.com>
2024-04-25 17:57:06 -0600

    v2 incomplete

--- models/livequery/silver_ordinals__get_transfers.py
+++ models/livequery/silver_ordinals__get_transfers.py
@@ -1,3 +1,4 @@
+import anyjson as json
 import snowflake.snowpark.types as T
 import snowflake.snowpark.functions as F
 
@@ -8,13 +9,13 @@ def model(dbt, session):
         incremental_strategy='merge',
         merge_exclude_columns = ['inserted_timestamp'],
         # unique_key='_RES_ID',
-        packages=['snowflake-snowpark-python'],
+        packages=['anyjson', 'snowflake-snowpark-python'],
         tags=["hiro_api"]
     )
 
     # Constants
     STARTING_HEIGHT = 767430
-    BATCH_SIZE = 1
+    BLOCK_RANGE = 1
     HIRO_LIMIT = 60
     VAULT_PATH = 'vault/dev/bitcoin/hiro'
 
@@ -29,13 +30,14 @@ def model(dbt, session):
         # Get distinct block_numbers from dbt.this()
         block_numbers = session.sql(f"SELECT DISTINCT block_number FROM {dbt.this}").collect()
         from_block_number = max([row[0] for row in block_numbers])
-        to_block_number = from_block_number + BATCH_SIZE
+        to_block_number = from_block_number + BLOCK_RANGE
     
     else:
         from_block_number = STARTING_HEIGHT
-        to_block_number = from_block_number + BATCH_SIZE
+        to_block_number = from_block_number + BLOCK_RANGE
 
 
+    # Load reference table
     transfers_per_block = dbt.ref('silver_ordinals__transfers_per_block').select(
         'block_number',
         'block_hash',
@@ -43,18 +45,34 @@ def model(dbt, session):
         ).where(
             (F.col('block_number') >= from_block_number) & (F.col('transfer_count') > 0)
         ).order_by(
-            'block_number').limit(BATCH_SIZE).collect()
+            'block_number').limit(BLOCK_RANGE).collect()
     
+    # Build Result DataFrame with schema: block_number, block_hash, offset, request_id, response, status_code, _request_timestamp
+    schema = T.StructType([
+        T.StructField('block_number', T.IntegerType()),
+        T.StructField('block_hash', T.StringType()),
+        T.StructField('offset', T.IntegerType()),
+        T.StructField('request_id', T.StringType()),
+        T.StructField('response', T.VariantType()),
+        # T.StructField('status_code', T.IntegerType()),
+        # T.StructField('_request_timestamp', T.TimestampType())
+    ])
+    
+    result_df = session.create_dataframe(
+        [],
+        schema
+    )
+
     while True:
         offset = 0
 
         for row in transfers_per_block:
-            block_number = row[0]
-            block_hash = row[1]
-            transfer_count = row[2]
+            block_number = int(row[0])
+            block_hash = str(row[1])
+            transfer_count = int(row[2])
 
             while offset < transfer_count:
-                url = f"{base_url}?block={block_hash}&offset={offset}&limit={HIRO_LIMIT}"
+                url = f"{base_url}?block={block_hash}&offset={str(offset)}&limit={str(HIRO_LIMIT)}"
 
                 livequery_sql = f"""
                     SELECT bitcoin.live.udf_api(
@@ -67,10 +85,26 @@ def model(dbt, session):
                 """
 
                 response = session.sql(livequery_sql)
-                res_value = response.collect()
+                res_value = json.loads(response.collect()[0][0])
 
-                return response
+                if res_value is not None:
+                    result_df = result_df.union(
+                        session.create_dataframe(
+                            [
+                                [
+                                    block_number,
+                                    block_hash,
+                                    offset,
+                                    str(block_number) + '-' + str(offset),
+                                    res_value,
+                                    # res_value['status_code'],
+                                    # F.sysdate()
+                                ]
+                            ],
+                            schema
+                        )
+                    )
 
+                    offset += HIRO_LIMIT
 
-
-    return False
+    return result_df
