4116d72394c998f0d4884e1a6b94e3a97c80cb7c
Jack Forgash <58153492+forgxyz@users.noreply.github.com>
2024-11-14 17:40:26 -0700

    api integration, udfs, dbt_project.yml config

--- macros/streamline/api_integrations.sql
+++ macros/streamline/api_integrations.sql
@@ -1,15 +1,15 @@
 {% macro create_aws_bitcoin_api() %}
     {% if target.name == "prod" %}
         {% set sql %}
-        CREATE api integration IF NOT EXISTS aws_bitcoin_api api_provider = aws_api_gateway api_aws_role_arn = 'arn:aws:iam::490041342817:role/bitcoin-api-prod-rolesnowflakeudfsAF733095-BXQYHR6NEDUG' api_allowed_prefixes = (
-            'https://xgp8ztpp0b.execute-api.us-east-1.amazonaws.com/prod/'
+        CREATE api integration IF NOT EXISTS aws_bitcoin_api api_provider = aws_api_gateway api_aws_role_arn = 'arn:aws:iam::924682671219:role/bitcoin-api-prod-rolesnowflakeudfsAF733095-D1PYK9UQXd8m' api_allowed_prefixes = (
+            'https://6fmgkfdhy6.execute-api.us-east-1.amazonaws.com/prod/'
         ) enabled = TRUE;
         {% endset %}
         {% do run_query(sql) %}
     {% else %}
         {% set sql %}
-        CREATE api integration IF NOT EXISTS aws_bitcoin_dev_api api_provider = aws_api_gateway api_aws_role_arn = 'arn:aws:iam::490041342817:role/bitcoin-api-dev-rolesnowflakeudfsAF733095-15K9O939DPMBA' api_allowed_prefixes = (
-            'https://f81vesdos6.execute-api.us-east-1.amazonaws.com/dev/'
+        CREATE api integration IF NOT EXISTS aws_bitcoin_dev_api api_provider = aws_api_gateway api_aws_role_arn = 'arn:aws:iam::704693948482:role/bitcoin-api-stg-rolesnowflakeudfsAF733095-Kp1QvE8vqElN' api_allowed_prefixes = (
+            'https://zta5i3yyxd.execute-api.us-east-1.amazonaws.com/stg/'
         ) enabled = TRUE;
         {% endset %}
         {% do run_query(sql) %}
